5df80f32a58f2173d6722cbdb161105b005f1611
sebasre18@gmail.com <sebasre18@gmail.com@1a1fd6f6-9d10-f096-0594-8339da851f06>
2012-12-04 17:09:10 +0000

    Agregue las entidades que me tocaban. En Historial de logueo y Lugar de realizacion esta como comentario un par de consultas/dudas.

---
+++ trunk/PruebaPantallas/Clases de entidad/Lugar de realizacion.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TPdeDiseño.Clases_de_entidad
+{
+    public class LugarDeRealizacion
+    {
+        //Atributos
+        private int codigo;
+        private string nombre;
+        private string descripcion;
+        //private int disponibilidad; ---> NOSE SI TIENE QUE IR YA QUE ESTA COMO OTRA ENTIDAD.... igualmente esta toda declarada aca como comentario.
+        private List<LugarDeRealizacion> lugaresDeRealizacion;
+
+        //Propiedades
+        public int _codigo
+        {
+            get
+            {
+                return codigo;
+            }
+            set
+            {
+                codigo = value;
+            }
+        }
+        public string _nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                nombre = value;
+            }
+        }
+        public string _descripcion
+        {
+            get
+            {
+                return descripcion;
+            }
+            set
+            {
+                descripcion = value;
+            }
+        }
+/*        public int _disponibilidad
+        {
+            get
+            {
+                return disponibilidad;
+            }
+            set
+            {
+                disponibilidad = value;
+            }
+        }*/
+        public List<Factor> _factores
+        {
+            get
+            {
+            
+            return this.factores;
+            }
+            set
+            {
+                this.factores = value;
+            }
+        }
+        public List<LugarDeRealizacion> _lugaresDeRealizacion
+        {
+            get
+            {
+                return this.lugaresDeRealizacion;
+            }
+            set
+            {
+                this.lugaresDeRealizacion = value;
+            }
+        }
+
+        //Constructor
+        public LugarDeRealizacion(int unCodigo, string unNombre, string unaDescripcion,/* int unaDisponibilidad,*/ List<LugarDeRealizacion> unaListaDeLugaresDeRealizacion)
+        {
+            this._codigo = unCodigo;
+            this._nombre = unNombre;
+            this._descripcion = unaDescripcion;
+            /*this._disponibilidad = unaDisponibilidad;*/
+            this._lugaresDeRealizacion = unaListaDeLugaresDeRealizacion;
+        }
+
+    }
+}
---
+++ trunk/PruebaPantallas/Clases de entidad/Provincia.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TPdeDiseño.Clases_de_entidad
+{
+    public class Provincia
+    {
+        //Atributos
+        private string id_provincia;
+        private string nombre;
+        private List<Provincia> provincias;
+
+        //Propiedades
+        public string _id_provincia
+        {
+            get
+            {
+                return id_provincia;
+            }
+            set
+            {
+                id_provincia = value;
+            }
+        }
+        public string _nombre
+        {
+            get
+            {
+                return nombre;
+            }
+            set
+            {
+                nombre = value;
+            }
+        }
+        public List<Provincia> _provincias
+        {
+            get
+            {
+                return this.provincias;
+            }
+            set
+            {
+                this.provincias = value;
+            }
+        }
+
+        //Constructores
+        public Provincia(string unId_provincia, string unNombre, List<Provincia> unaListaDeProvincias)
+        {
+            this._id_provincia = unId_provincia;
+            this._nombre = unNombre;
+            this._provincias = unaListaDeProvincias;
+        }
+    }
+}
