e4fd79c9c5de0bde025033f2aa77b6eab9a0287e
sebasre18@gmail.com <sebasre18@gmail.com@1a1fd6f6-9d10-f096-0594-8339da851f06>
2012-12-08 16:13:18 +0000

    Agregue la lista de partidos a la entidad.

--- PruebaPantallas/Clases de entidad/Ronda.cs
+++ PruebaPantallas/Clases de entidad/Ronda.cs
@@ -8,8 +8,9 @@ namespace TPdeDiseño.Clases_de_entidad
     class Ronda
     {
         //Atributos
-        private int nro_ronda;
+        private int nro_ronda; 
         private string nombre;
+        private List<Partido> partidos;
 
         //Propiedades
         public int _nro_ronda
@@ -34,14 +35,26 @@ namespace TPdeDiseño.Clases_de_entidad
                 nombre = value;
             }
         }
+        public List<Partido> _partidos
+        {
+            get
+            {
+                return partidos;
+            }
+            set
+            {
+                partidos = value;
+            }
+        }
 
         //Constructor
         public Ronda() { }
         
-        public Ronda(int unNro_ronda, string unNombre)
+        public Ronda(int unNro_ronda, string unNombre, List<Partido> listaDePartidos)
         {
-            this._id_deporte = unNro_ronda;
+            this._nro_ronda = unNro_ronda;
             this._nombre = unNombre;
+            this._partidos = listaDePartidos;
         }
     }
 }
